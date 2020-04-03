875fc78671ec0556ee1eaaa1b947c48da1f1ac92
Christopher Dougherty <christophertdoc@gmail>
2020-04-02 20:54:01 -0400

    lesson-3

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ Based on the following tutorials starting at video #34: https://www.youtube.com/
 ## Branches
 - lesson-1: display data from central store by using "mapStateToProps()"
     1. In particular, we display a list of blog posts on the homepage.
-- lesson-2: display redux data of an individual post on its own page
+- lesson-2: display global data for an individual post on its own page
+- lesson-3: updating the global redux data with mapDispatchToProps
 
 ## Terms
 - Redux: central data store for all app data (global state)
--- src/components/Post.js
+++ src/components/Post.js
@@ -2,17 +2,23 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 class Post extends Component {
+
+    handleClick = () => {
+        this.props.deletePost(this.props.post.id)
+        this.props.history.push('/')
+    }
+
 	render() {
+        console.log(this.props)
         const post = this.props.post ? (
             <div className='post'>
                 <h2>{ this.props.post.title }</h2>
                 <p>{ this.props.post.body }</p>
+                <button onClick={this.handleClick}>DELETE POST</button>
             </div>
         ) : (
             <h3>Loading...</h3>
         )
-
-
 		return (
             <div className='Post'>
                 { post }
@@ -28,4 +34,10 @@ const mapStateToProps = (state, ownProps) => {
     }
 }
 
-export default connect(mapStateToProps)(Post)
+const mapDispatchToProps = (dispatch) => {
+    return {
+        deletePost: (id) => { dispatch({type: 'DELETE_POST', id: id}) }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Post)
